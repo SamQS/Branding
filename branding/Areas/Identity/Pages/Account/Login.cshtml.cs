d399b8e83327d05b12cc3f07792b1c29c7608c36
Samuel Quenta <diegodsqs2@gmail.com>
2021-05-24 19:23:24 -0500

    Cambios Errores Registrar y Login

--- branding/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ branding/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -50,7 +50,7 @@ namespace branding.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Recuerdame?")]
             public bool RememberMe { get; set; }
         }
 
@@ -82,7 +82,7 @@ namespace branding.Areas.Identity.Pages.Account
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User logged in.");
+                    _logger.LogInformation("Usuario Conectado");
                     return LocalRedirect(returnUrl);
                 }
                 if (result.RequiresTwoFactor)
@@ -96,7 +96,7 @@ namespace branding.Areas.Identity.Pages.Account
                 }
                 else
                 {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    ModelState.AddModelError(string.Empty, "Intento de inicio de sesión no válido");
                     return Page();
                 }
             }
--- branding/Models/Consulta.cs
+++ branding/Models/Consulta.cs
@@ -2,9 +2,13 @@ namespace branding.Models
 {
     public class Consulta
     {
+        
         public int Id {get; set; }
+
         public string Nombre {get; set; }
+
         public string Email {get; set; }
+
         public string Consult {get; set; }
     }
 }
